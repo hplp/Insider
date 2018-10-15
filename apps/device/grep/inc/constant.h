990efddb35790864f5176481e395012c486686a2
Zhenyuan Ruan <zainryan95@gmail.com>
2018-10-15 21:42:55 +0000

    Move all device code into a separate device folder

---
+++ apps/device/grep/inc/constant.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANT_H_
+#define CONSTANT_H_
+
+#define MAX_GREP_PARAM_SIZE (32)
+#define POKE_WIDTH_BYTES (4)
+#define COMPARE_CHUNK_SIZE (4)
+#define MATCHING_VEC_SIZE (DATA_BUS_WIDTH + MAX_GREP_PARAM_SIZE - 1)
+
+#endif
