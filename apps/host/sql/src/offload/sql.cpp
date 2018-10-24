eda3958e257ca7aba385af6c232483551ccea83c
Zhenyuan Ruan <zainryan95@gmail.com>
2018-10-24 17:35:16 +0000

    Remove the redundant code block.

--- apps/host/sql/src/offload/sql.cpp
+++ apps/host/sql/src/offload/sql.cpp
@@ -135,31 +135,12 @@ void read_data() {
   iclose(fd);
 }
 
-void execute() {
-  // for (Record *ptr = record_arr; ptr != record_arr_ptr; ptr ++) {
-  //     int id = get_round_name_mapping_id(string(ptr -> round_name, ROUND_NAME_LEN));
-  //     calc_scores[id].sum += ptr -> score;
-  //     calc_scores[id].num ++;
-  // }
-  // for (int i = 1; i < ROUND_NUM; i ++) {
-  //     string str = mappings_int_str[i];
-  //     if (str != "") {
-  //         int id = get_round_name_mapping_id(str);
-  //         //cout << str << " " << calc_scores[id].sum / calc_scores[id].num << endl;
-  //     }
-  // }
-}
-
 int main(int argc, char **argv) {
   puts("read_data().....");
   Timer timer;
   read_data();
   cout << "elapsed time = " << timer.elapsed() / 1000 << endl;
-
-  puts("execute().....");
-  timer.reset();
-  execute();
-  cout << "elapsed time = " << timer.elapsed() / 1000 << endl;
   cout << "total read bytes is " << totalReadBytes << endl;
+
   return 0;
 }
